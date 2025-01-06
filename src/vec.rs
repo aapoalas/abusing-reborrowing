df156e84406d6279c0013a7b0acf03a1dec9ce07
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-01-06 15:08:45 +0200

    vec: Add &mut and & usage together and interprocedurally

--- src/vec.rs
+++ src/vec.rs
@@ -1,6 +1,8 @@
 //! Borrowing values through vectors.
 
+#[allow(clippy::needless_lifetimes)]
 fn act<'a>(vec: &'a mut Vec<u32>) {                     // <--              + 'a lifetime
+    use_mut_and_ref_together(vec);                      //                  |
     let first_value = add(vec, rand::random());         // <-- + '1 starts  |
     println!("First value: {}", first_value);           //     | '1 ends    |
     let second_value = add(vec, rand::random());        // <-- / '2 starts  |
@@ -18,6 +20,27 @@ fn add(vec: &mut Vec<u32>, value: u32) -> &u32 {
 
 fn clean(vec: &mut Vec<u32>) {
     vec.retain(|_| rand::random::<bool>());
+}
+
+fn use_mut_and_ref_together(vec: &mut Vec<u32>) {
+    add(vec, rand::random());
+    let first_value = vec.first().unwrap();
+    println!("&'a mut Vec<u32>: {:?}; &'a u32: {}", vec, first_value);
+    add(vec, rand::random());
+    use_mut_and_ref_together_interprocedurally(vec);
+}
+
+fn use_mut_and_ref_together_interprocedurally(vec: &mut Vec<u32>) {
+    add(vec, rand::random());
+    #[allow(unused)]
+    let first_value = vec.first().unwrap();
+    // interprocedural_print_and_add(vec, first_value);
+}
+
+#[allow(unused)]
+fn interprocedural_print_and_add(vec: &mut Vec<u32>, first_value: &u32) {
+    println!("&'a mut Vec<u32>: {:?}; &'a u32: {}", vec, first_value);
+    add(vec, rand::random());
 }
 
 pub(crate) fn start() {
